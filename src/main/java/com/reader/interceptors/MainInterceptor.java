0a24061ee8a49e2c034c267374f3c577c6142154
bobis1888 <bobis1888@gmail.com>
2021-08-25 23:42:57 +0500

    ref

--- src/main/java/com/reader/interceptors/MainInterceptor.java
+++ src/main/java/com/reader/interceptors/MainInterceptor.java
@@ -1,8 +1,7 @@
 package com.reader.interceptors;
 
-import org.apache.catalina.connector.ResponseFacade;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,9 +12,13 @@ import javax.servlet.http.HttpServletResponse;
 @Component
 public class MainInterceptor implements HandlerInterceptor {
 
+    Logger logger = LoggerFactory.getLogger(MainInterceptor.class);
+
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         var origin = request.getHeader("Origin");
+        logger.info("Request from : " + origin);
+        logger.info("Request destination : " + request.getRequestURI());
         response.addHeader("Access-Control-Allow-Origin",origin);
         response.addHeader("Vary","Origin");
         return true;
@@ -23,6 +26,6 @@ public class MainInterceptor implements HandlerInterceptor {
 
     @Override
     public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception {
-        System.out.println("Response status : " + response.getStatus());
+        logger.info("Response status : " + response.getStatus());
     }
 }
--- src/main/java/com/reader/services/ItemService.java
+++ src/main/java/com/reader/services/ItemService.java
@@ -1,6 +1,8 @@
 package com.reader.services;
 
 import com.reader.models.Item;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
@@ -11,6 +13,8 @@ import java.util.stream.Collectors;
 
 @Component
 public class ItemService {
+
+    Logger logger = LoggerFactory.getLogger(ItemService.class);
 
     private List<Item> items;
 
